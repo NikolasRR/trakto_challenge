66c8b4ff5ec361d7d4aee81cae3b605681c0a404
Nikolas <nkls99@gmail.com>
2023-04-25 22:37:35 -0300

    sing-in screen layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignInScreenComponent } from './sign-in-screen/sign-in-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'sign-in', component: SignInScreenComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign-in-screen/sign-in-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-screen',
+  templateUrl: './sign-in-screen.component.html',
+  styleUrls: ['./sign-in-screen.component.css']
+})
+export class SignInScreenComponent {
+
+}
